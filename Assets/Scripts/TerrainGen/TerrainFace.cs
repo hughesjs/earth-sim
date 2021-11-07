ebb363f9647e0574abb7b57d6318824c29473e2d
James Hughes <james@pyrosoftsolutions.co.uk>
2021-11-07 23:49:05 +0000

    Deleted old mesh-generator and refactored terrainface to separate vertex and triangle loops

--- Assets/Scripts/TerrainGen/TerrainFace.cs
+++ Assets/Scripts/TerrainGen/TerrainFace.cs
@@ -1,15 +1,15 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace TerrainGen
 {
     public class TerrainFace
     {
-        private Mesh _mesh;
-        private int _resolution;
-        private Vector3 _localUp;
-        private Vector3 _axisA;
-        private Vector3 _axisB;
+        private readonly Mesh _mesh;
+        private readonly int _resolution;
+        private readonly Vector3 _localUp;
+        private readonly Vector3 _axisA;
+        private readonly Vector3 _axisB;
 
         public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
         {
@@ -23,17 +23,26 @@ namespace TerrainGen
 
         public void ConstructMesh()
         {
-            Vector3[] vertices = new Vector3[_resolution * _resolution];
-            int totalTris = 6 * (_resolution - 1) * (_resolution - 1);
-            int[] tris = new int[totalTris];
+            List<Vector3> vertices = CreateVertices();
+            List<int> triMap = CreateTriMap();
 
-            for (int y = 0, i = 0, t = 0; y < _resolution; y++)
+            _mesh.Clear();
+            _mesh.vertices = vertices.ToArray();
+            _mesh.triangles = triMap.ToArray();
+            _mesh.RecalculateNormals();
+        }
+
+        private List<Vector3> CreateVertices()
+        {
+            List<Vector3> vertices = new(_resolution * _resolution);
+            
+            for (int y = 0; y < _resolution; y++)
             {
-                for (int x = 0; x < _resolution; x++, i++)
+                for (int x = 0; x < _resolution; x++)
                 {
                     Vector2 ratio = new Vector2(x, y) / (_resolution - 1);
                     // Start at origin, need values between (-1,1,-1) and (1,1,1)
-                    // So we need to go up one, subtract a half to get on the line
+                    // So we need to go up one and subtract a half to get on the line
                     // and then add twice the progress along the axis line
                     // (it's twice because there's a distance of 2 between -1 and 1)
                     Vector3 pointOnUnitCube = _localUp +
@@ -41,25 +50,38 @@ namespace TerrainGen
                                               ((ratio.y - .5f) * 2 * _axisB);
 
                     Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
-                    
-                    vertices[i] = pointOnUnitSphere;
 
-                    if (x < _resolution - 1 && y < _resolution - 1)
-                    {
-                        tris[t++] = i;
-                        tris[t++] = i + _resolution + 1;
-                        tris[t++] = i + _resolution;
-                        tris[t++] = i;
-                        tris[t++] = i + 1;
-                        tris[t++] = i + _resolution + 1;
-                    }
+                    vertices.Add(pointOnUnitSphere);
                 }
             }
+            return vertices;
+        }
 
-            _mesh.Clear();
-            _mesh.vertices = vertices;
-            _mesh.triangles = tris;
-            _mesh.RecalculateNormals();
+        private List<int> CreateTriMap()
+        {
+            int totalTris = 6 * (_resolution - 1) * (_resolution - 1);
+            List<int> triMap = new(totalTris);
+            for (int y = 0, i = 0; y < _resolution; y++) 
+            {
+                for (int x = 0; x < _resolution; x++, i++)
+                {
+                    if (x >= _resolution - 1 || y >= _resolution - 1) // Last row and column would be mapping off-mesh
+                    {
+                        continue;
+                    }
+
+                    triMap.Add(i);
+                    triMap.Add(i + _resolution + 1);
+                    triMap.Add(i + _resolution);
+                    triMap.Add(i);
+                    triMap.Add(i + 1);
+                    triMap.Add(i + _resolution + 1);
+                }
+            }
+            return triMap;
         }
     }
 }
+       
+    
+
